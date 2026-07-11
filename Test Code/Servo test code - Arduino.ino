dffb83f587a5c34234bf0ee09b238532bdc5a645
Swasti Kedia <75873332+Swasti38@users.noreply.github.com>
2026-07-11 17:30:22 +0530

    Add servo control functionality with serial input

--- Test Code/Servo test code - Arduino.ino
+++ Test Code/Servo test code - Arduino.ino
@@ -1,1 +1,30 @@
+#include <Servo.h>
+Servo servo;
+int currentAngle = 90;
 
+void setup() {
+  Serial.begin(9600);
+  servo.attach(9);
+  servo.write(90);
+  Serial.println("Arduino connected!");
+  Serial.println("Use this port for Python:");
+}
+
+void loop() {
+  if (Serial.available()) {
+    int targetAngle = Serial.parseInt();
+    targetAngle = constrain(
+      targetAngle,
+      0,
+      180
+    );
+    if (currentAngle < targetAngle) {
+      currentAngle++;
+    }
+    else if (currentAngle > targetAngle) {
+      currentAngle--;
+    }
+    servo.write(currentAngle);
+  }
+  delay(15);
+}
